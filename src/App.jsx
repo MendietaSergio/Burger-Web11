6d689339355b1ca4771f1c488fc2d729cdccbcaa
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-28 09:38:35 -0300

    Añade inicializacion del proyecto, agrega script bootstrap, fontawesome, sweetalert2

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import react, { useState } from 'react'
+function App() {
+  return (
+    <div className="container-fluid">
+      <h1 className='text-center'>Burger Web</h1>
+    </div>
+  )
+}
+
+export default App
