1c3b621112d2ff64cb88ec45a69bf172b5118839
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-29 01:56:19 -0300

    añade home en app.jsx, para visualizar resultados

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import react, { useState } from 'react'
 import { Header } from './components/Header/Header'
+import { Home } from './pages/Home'
 function App() {
   return (
     <div className="container-fluid">
       <Header/>
+      <Home/>
     </div>
   )
 }
