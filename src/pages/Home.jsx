2b0ceb69325a71979c0c3763fee0a583bf66eb10
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-29 01:53:46 -0300

    Añade carpeta 'pages' donde estaran las paginas

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { OptionsSelection } from '../components/OptionsSelection/OptionsSelection'
+import { PortBurger } from '../components/PortBurger/PortBurger'
+
+export const Home = () => {
+  return (
+    <div>
+      <PortBurger/>
+      <OptionsSelection/>
+    </div>
+  )
+}
