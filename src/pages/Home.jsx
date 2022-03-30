3904b7214fd1515a5541b3e7df2da2b2b78d3dd2
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-30 00:52:25 -0300

    Añade componentes nuevos a la pagina home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Banner } from '../components/Banner/Banner'
+import { FeaturedProduct } from '../components/FeaturedProduct/FeaturedProduct'
+import { Footer } from '../components/Footer/Footer'
+import { HappyCustomers } from '../components/HappyCustomers/HappyCustomers'
 import { OptionsSelection } from '../components/OptionsSelection/OptionsSelection'
 import { PortBurger } from '../components/PortBurger/PortBurger'
 
@@ -7,6 +11,10 @@ export const Home = () => {
     <div>
       <PortBurger/>
       <OptionsSelection/>
+      <FeaturedProduct/>
+      <Banner/>
+      <HappyCustomers/>
+      <Footer/>
     </div>
   )
 }
