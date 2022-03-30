a51810492842f0724177c7535eb33888bb8a7808
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-30 00:49:17 -0300

    añade componente 'happycustomers' donde estan los comentarios de los clientes

---
+++ src/components/HappyCustomers/HappyCustomers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CardsComments } from '../CardsComments/CardsComments'
+import { Title } from '../Title/Title'
+
+export const HappyCustomers = () => {
+    return (
+        <>
+        <Title title="Clientes Felices"/>
+        <CardsComments/>
+        </>
+    )
+}
