4db8f2bb538c8134542138d6a12bbba587442303
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-30 00:51:03 -0300

    añade carta de comentario con el texto, img del cliente, estrella, nombre del cliente

---
+++ src/components/CardsComments/CardsComments.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import './CardsComments.css'
+export const CardsComments = () => {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className="col-12 col-md-4">
+                    <div className='box-comments my-3'>
+                        <span>
+                            "Muy ricas las Burgers y buena la atención en el local."
+                        </span>
+                        <span className='text-left'>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </span>
+                        <div className='box-img'>
+                            <img className='float-left' src="https://parisburger.com.ar/wp-content/uploads/2021/07/client02-free-img.jpg" alt="foto perfil" />
+                            <span>Daiana Benencia</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-4">
+                    <div className='box-comments my-3'>
+                        <span>
+                            "Muy ricas las Burgers y buena la atención en el local."
+                        </span>
+                        <span className='text-left'>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </span>
+                        <div className='box-img'>
+                            <img className='float-left' src="https://parisburger.com.ar/wp-content/uploads/2021/07/client02-free-img.jpg" alt="foto perfil" />
+                            <span>Daiana Benencia</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-4">
+                    <div className='box-comments my-3'>
+                        <span>
+                            "Muy ricas las Burgers y buena la atención en el local."
+                        </span>
+                        <span className='text-left'>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </span>
+                        <div className='box-img'>
+                            <img className='float-left' src="https://parisburger.com.ar/wp-content/uploads/2021/07/client02-free-img.jpg" alt="foto perfil" />
+                            <span>Daiana Benencia</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
