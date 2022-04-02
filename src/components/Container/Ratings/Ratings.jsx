d3a416f98b9c624d2e9f5a7b0c22c3691ac711aa
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-02 08:59:49 -0300

    Añade info en ratings

--- src/components/Container/Ratings/Ratings.jsx
+++ src/components/Container/Ratings/Ratings.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export const Ratings = () => {
+export const Ratings = ({ setShowLine, showLine }) => {
   return (
-    <div>Ratings</div>
+    <div className="col-12">
+      <h4>Valoraciones</h4>
+      <small>No hay valoraciones</small>
+      <div className='detail-warning'>
+        <h5>Sé el primero en valorar "Burger ELYSÉE"</h5>
+        <span>Debes <a href='/login'>acceder</a> para publicar una reseña</span>
+      </div>
+    </div>
   )
 }
