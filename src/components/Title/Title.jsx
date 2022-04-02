dd4b5583b300b749d9dfce59fd1d26e13f91c2bb
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-02 15:46:32 -0300

    añade props classname

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Title.css'
-export const Title = ({ title="", bar=true }) => {
+export const Title = ({ title="", bar=true, className="text-center" }) => {
     return (
         <div className='row'>
             <div className='col-12'>
-                <h1 className='text-center my-3'>{title}</h1>
+                <h1 className={`${className}my-3`}>{title}</h1>
                 {
                     bar ? (
                         <hr className='title-bar' />
