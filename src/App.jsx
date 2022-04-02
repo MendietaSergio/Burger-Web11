ac0057a74a60679c2ca309652102e74de5846153
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-02 08:43:53 -0300

    Añade ruta /productos /productos/:iddetail

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import react, { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import { Header } from './components/Header/Header'
 import { Home } from './pages/Home'
+import { Productos } from './pages/Productos'
+import { Footer } from './components/Footer/Footer'
+import { Detail } from './pages/Detail'
+
 function App() {
   return (
-    <div className="container-fluid">
-      <Header/>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <div className="container-fluid">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/productos' element={<Productos />} />
+          <Route path='/productos/:idDetail' element={<Detail />} />
+
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
 
