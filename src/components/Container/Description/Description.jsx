489660026be902253f5bec8b5c49f1b0b2a50fbd
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-02 08:58:37 -0300

    Añade info de descripcion del detalle

---
+++ src/components/Container/Description/Description.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Description = ({ setShowLine, showLine }) => {
+  return (
+    <div className="col-12">
+      <span className='detail-categorie'>papas rusticas caseras incluidas</span>
+      <div className='detail-list'>
+        <ul>
+          <li>mac cheese</li>
+          <li>panceta</li>
+          <li>salsa bbq</li>
+          <li>200g de carne</li>
+          <li>deep fried</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
