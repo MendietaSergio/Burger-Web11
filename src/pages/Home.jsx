84d8be759a221b5c6f2800010c13c3d555601fb5
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-02 08:43:14 -0300

    Añade componentes de comentarios de clientes felices

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Banner } from '../components/Banner/Banner'
 import { FeaturedProduct } from '../components/FeaturedProduct/FeaturedProduct'
-import { Footer } from '../components/Footer/Footer'
 import { HappyCustomers } from '../components/HappyCustomers/HappyCustomers'
 import { OptionsSelection } from '../components/OptionsSelection/OptionsSelection'
 import { PortBurger } from '../components/PortBurger/PortBurger'
@@ -14,7 +13,6 @@ export const Home = () => {
       <FeaturedProduct/>
       <Banner/>
       <HappyCustomers/>
-      <Footer/>
     </div>
   )
 }
