88bc2ea990b1a1282e6bd5b2f4f2391baf442de4
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-02 08:42:23 -0300

    añade mejoras en el componente 'title'

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Title.css'
-export const Title = ({ title="" }) => {
+export const Title = ({ title="", bar=true }) => {
     return (
         <div className='row'>
             <div className='col-12'>
                 <h1 className='text-center my-3'>{title}</h1>
-                <hr className='title-bar' />
+                {
+                    bar ? (
+                        <hr className='title-bar' />
+                    ):null
+                }
             </div>
         </div>
     )
