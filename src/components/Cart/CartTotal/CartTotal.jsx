100fa2ff27b5e4f84620c4807f15288ef2a260bf
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-03 18:21:40 -0300

    Añade carttotal donde aparece el monto total de los item del carrito

--- src/components/Cart/CartTotal/CartTotal.jsx
+++ src/components/Cart/CartTotal/CartTotal.jsx
@@ -5,18 +5,20 @@ export const CartTotal = () => {
         <div className='col-12'>
             <div className="container-cartTotal">
                 <div className="container-cartTotal-section">
-                    <h5 className=''>Total carrito</h5>
+                    <h5 className=''>Total del carrito</h5>
                 </div>
-                <div className="container-cartTotal-section">
-                    <span>Subtotal:</span>
-                    <span>$.1960,0</span>
-                </div>
-                <div className="container-cartTotal-section">
-                    <span>Envío:</span>
-                    <span></span>
-                </div>
-                <div className="container-cartTotal-section">
-                    <button type='submit' className='btn-toAccess' >Finalizar compra</button>
+                <div className='container-cartTotal-info'>
+                    <div className="">
+                        <span>Subtotal:</span>
+                        <span>$.1960,0</span>
+                    </div>
+                    <div className="">
+                        <span>Envío:</span>
+                        <span></span>
+                    </div>
+                    <div className="">
+                        <button type='submit' className='btn-toAccess' >Finalizar compra</button>
+                    </div>
                 </div>
             </div>
         </div>
