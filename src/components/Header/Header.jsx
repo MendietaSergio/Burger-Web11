1c1236006c1d1dc88d81702d5e72e043bd3fc6b6
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-11 10:01:38 -0300

    Añade enlaces redireccionales

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { CartModal } from '../Cart/CartModal/CartModal'
 import './Header.css'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 export const Header = () => {
     const [showCartList, setShowCartList] = useState(false)
+    const [menuCategorie, setMenuCategorie] = useState({})
+    const [loading, setLoading] = useState(false)
 
+    useEffect(() => {
+        const getCategories = async () => {
+            await axios.get('http://localhost:3001/api/products/categories')
+                .then((res) => {
+                    console.log("RES => ", res);
+                    setMenuCategorie(res.data)
+                })
+                .catch((error) => console.log(error))
+                .finally(() => {
+                    setLoading(true)
+                    console.log("state => ", menuCategorie)
+                })
+        }
+        getCategories()
+    }, [])
     return (
         <div className='row row-header'>
             <div className='container'>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="#">Burger</a>
+                    <Link className="navbar-brand" to="/">Burger</Link>
 
                     <div className="cart-icon-movil ml-4 my-2 my-lg-0">
                         0<i className="fas fa-shopping-cart" onClick={() => setShowCartList(!showCartList)}></i>
@@ -24,38 +43,39 @@ export const Header = () => {
                     <div className="collapse navbar-collapse " id="navbarSupportedContent">
                         <ul className="navbar-nav d-flex justify-content-end">
                             <li className="nav-item active">
-                                <a className="nav-link" href="/#">Inicio <span className="sr-only">(current)</span></a>
+                                <Link className="nav-link" to="/">Inicio <span className="sr-only">(current)</span></Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Productos</a>
+                                <Link className="nav-link" to="/productos">Productos</Link>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Burger's
                                 </a>
                                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a className="dropdown-item" href="#">Queso Cheddar</a>
-                                    <a className="dropdown-item" href="#">Queso Francés</a>
-                                    <a className="dropdown-item" href="#">New York</a>
-                                    <a className="dropdown-item" href="#">Veggie</a>
+                                    {loading ? (
+                                        menuCategorie.filter(categories => categories.nombre === 'Burger').map(categorie => (
+                                            <Link key={categorie.id} className="dropdown-item" to={`/productos/burger/${categorie.categoria}`}>{categorie.categoria}</Link>
+                                        ))
+                                    ) : (null)}
                                     <div className="dropdown-divider"></div>
-                                    <a className="dropdown-item" href="#">Ver todas</a>
+                                    <Link className="dropdown-item" to="#">Ver todas</Link>
                                 </div>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Papas y salsas</a>
+                                <Link className="nav-link" to="/productos/papasysalsas">Papas y salsas</Link>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Bebidas</a>
+                                <Link className="nav-link" to="/productos/bebida">Bebidas</Link>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Cuenta
                                 </a>
                                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a className="dropdown-item" href="#">Mi cuenta</a>
-                                    <a className="dropdown-item" href="#">Carrito</a>
+                                    <Link className="dropdown-item" to="/ingresar">Mi cuenta</Link>
+                                    <Link className="dropdown-item" to="/micarrito">Carrito</Link>
                                 </div>
                             </li>
                         </ul>
