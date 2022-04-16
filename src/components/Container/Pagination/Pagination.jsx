7d9cc812c25abe033ce0ab33a92ff2f43f9389f3
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-15 22:08:33 -0300

    Añade componente 'pagination', en base a la cantidad de productos

---
+++ src/components/Container/Pagination/Pagination.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Pagination = ({ paginate, totalProducts, productsPage }) => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalProducts / productsPage); i++) {
+        pageNumbers.push(i)
+    }
+
+    return (
+        <nav>
+            <ul className='pagination'>
+                {pageNumbers.map(number => (
+                    <li key={number} className="page-item">
+                        <a onClick={() => paginate(number)} className='page-link'>{number}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
