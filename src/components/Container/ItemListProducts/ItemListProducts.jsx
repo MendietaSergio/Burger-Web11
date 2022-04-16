efdf6dc01041f82a275c89abe71deeedf64b3e99
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-15 22:14:49 -0300

    Añade loading para mostrar los nuevos productos al cambiar de pag. agregar componente 'Card'

--- src/components/Container/ItemListProducts/ItemListProducts.jsx
+++ src/components/Container/ItemListProducts/ItemListProducts.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
+import { Card } from '../Card/Card'
 
-export const ItemListProducts = () => {
+export const ItemListProducts = ({ products, loading}) => {
+    if (loading) {
+        return <i className="fas fa-spinner fa-pulse"></i>
+    }
+
     return (
-        <div className='container-featuredProduct my-3'>
-            <div className='container-img-featuredProduct'>
-                <img className='img-featuredProduct' src="https://res.cloudinary.com/freelance01/image/upload/v1648526368/burger_web/burgers_option_jlzycs.jpg" alt="Burger" />
-                <div className='container-img-detail'>
-                    <h5 className='viewDetail'>Ver Detalle</h5>
-                </div>
-            </div>
-            <div className='container-info'>
-                <span className='title-categorie'>New York</span>
-                <h5 className=''>Burger WHOPBURGER</h5>
-                <span className='price'>$890,00</span>
-                <div className='btn-link'>
-                    <a className='optionsLink' href="#">Pedime Ahora <i className="fas fa-angle-right"></i></a>
-                </div>
-            </div>
-        </div>
+        <>
+            {
+                products.map(product => (
+                    <div className="col-12 col-md-3" key={product._id}>
+                        <Card product={product} />
+                    </div>
+                ))
+            }
+        </>
     )
 }
