ee8802dbd4cb2e9bc8ad437adda5e0578275d2d5
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-17 22:26:36 -0300

    Añade 'categoria', 'tipo' para mostrar dependiendo si tienen valor a mostrar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Navigation.css'
-export const Navigation = () => {
+export const Navigation = ({ categoria, tipo }) => {
+    console.log("categoria navigation ", categoria);
     return (
         <>
             <div className="container-navigation-page">
                 <span className='navigation-page'>
                     <Link to="/" >Home <i className="fas fa-chevron-right"></i></Link>
                     <Link to="/productos" >Productos <i className="fas fa-chevron-right"></i></Link>
-
+                    {categoria === undefined ? (null) : (
+                        <Link to={`/productos/${categoria}`} >{categoria}</Link>
+                    )}
+                    {tipo === undefined ? (null) : (
+                        <Link to={`/productos/${categoria}/${tipo}`} ><i className="fas fa-chevron-right"></i> {tipo}</Link>
+                    )}
                 </span>
             </div>
         </>
