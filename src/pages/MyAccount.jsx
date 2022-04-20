0555565317498fd5e55f196b4a62e81813914912
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-20 00:37:49 -0300

    Añade register si el estado es false

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Login } from '../components/Login/Login'
+import { Register } from '../components/Register/Register'
 
 export const MyAccount = () => {
+    const [viewRegister, setViewRegister] = useState('pepe')
     return (
         <div className='container'>
-            <Login />
+            {viewRegister ? (
+            <Login setViewRegister={setViewRegister} />
+            ):(<Register setViewRegister={setViewRegister}/>)}
         </div>
     )
 }
