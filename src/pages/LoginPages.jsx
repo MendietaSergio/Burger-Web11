73b1d36c27990674c32d3e32884185c4853d04c4
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-28 22:58:52 -0300

    Añade pagina de login

---
+++ src/pages/LoginPages.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from 'react'
+import { Login } from '../components/Login/Login'
+
+export const LoginPages = () => {
+
+    return (
+        <div className='container'>
+            <Login/>            
+        </div>
+    )
+}
