f6675a68429b4c254a309521b4aa8e6d55af7ebd
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-28 22:57:52 -0300

    Añade register con conexion a la api

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,45 +1,102 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { Link, Navigate } from 'react-router-dom'
+import { validations } from '../../utils/ValidationsRegister'
+import { Message } from '../Message/Message'
 import { Title } from '../Title/Title'
 import './Register.css'
-export const Register = ({ setViewRegister }) => {
+
+export const Register = () => {
+
+  const { register, handleSubmit, reset, formState: { errors } } = useForm()
+  const [viewMessage, setViewMessage] = useState(false)
+  const [message, setMessage] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [succes, setSucces] = useState(false)
+  const submit = async (data) => {
+    setViewMessage(false)
+    setMessage('')
+    const { nombre, usuario, email, password } = data;
+    await axios.post('http://localhost:3001/api/auth/signup', {
+      nombre,
+      usuario,
+      email,
+      password
+    })
+      .then(res => {
+        setLoading(!loading)
+        if (res.data.ok) {
+          setTimeout(() => {
+            setSucces(true)
+            setLoading(false)
+          }, 2000)
+
+        } else {
+          setLoading(!loading)
+          setMessage(res.data.message)
+          setTimeout(() => {
+            setViewMessage(true)
+            setLoading(false)
+          }, 2000)
+        }
+      })
+  }
+
   return (
-    <div className="container-form">
-      <div className="container-form-body">
-        <Title title={"Registro"} bar={false} />
+    <>
+      {succes && <Navigate to="/micuenta" />}
+      <div className="container-form">
+        <div className="container-form-body">
+          <Title title={"Registro"} bar={false} />
+          {viewMessage ?
+            (<Message message={message} viewMessage={viewMessage} setViewMessage={setViewMessage} />) : (null)
+          }
+        </div>
+        <form onSubmit={handleSubmit(submit)} className="form-login">
+          <div className='row'>
+            <div className='col-12  col-md-6'>
+              <label name="nombre">Nombre completo <small>*</small> </label>
+              <input name="nombre" className={errors.nombre ? ("form-control is-invalid") : ("form-control")} type="text" {...register('nombre', validations.nombre)} />
+              {errors.nombre ? <small className='text-danger'>{errors.nombre.message}</small> : null}
+            </div>
+            <div className='col-12  col-md-6'>
+              <label name="usuario">Usuario <small>*</small> </label>
+              <input name="usuario" className={errors.usuario ? ("form-control is-invalid") : ("form-control")} type="text" {...register('usuario', validations.usuario)} />
+              {errors.usuario ? <small className='text-danger'>{errors.usuario.message}</small> : null}
+            </div>
+            <div className='col-12'>
+              <label name="email">Email <small>*</small> </label>
+              <input name="email" className={errors.email ? ("form-control is-invalid") : ("form-control")} type="email" {...register('email', validations.email)} />
+              {errors.email ? <small className='text-danger'>{errors.email.message}</small> : null}
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-12 col-md-6'>
+              <label name="password">Contraseña <small>*</small></label>
+              <input name="password" className={errors.password ? ("form-control is-invalid") : ("form-control")} type="password" {...register('password', validations.password)} />
+              {errors.password ? <small className='text-danger'>{errors.password.message}</small> : null}
+            </div>
+          </div>
+          <div className='d-flex justify-content-start align-items-baseline'>
+            <input name="rememberMe" type="checkbox" />
+            <label htmlFor='rememberMe' name="rememberMe" className='px-2'>Acepto termino y condiciones</label>
+          </div>
+          <div className='d-flex flex-row justify-content-around'>
+            <div>
+              <button type='submit' className='btn-toRegister' >Registarme
+                {loading ? (<i className="fas fa-spinner fa-pulse"></i>) : null}
+              </button>
+            </div>
+            <div>
+              <button type='submit' className='btn-toCancel' >Cancelar</button>
+            </div>
+          </div>
+          <div>
+            <small >¿Tenes una cuenta? <Link className='info-register' to='/ingresar'>¡Logeate!</Link></small>
+          </div>
+        </form>
       </div>
-      <form action="" className="form-login">
-        <div>
-          <label name="nombre">Nombre <small>*</small> </label>
-          <input name="nombre" className="form-control" type="text" />
-        </div>
-        <div>
-          <label name="apellido">Apellido <small>*</small> </label>
-          <input name="apellido" className="form-control" type="text" />
-        </div>
-        <div>
-          <label name="password">Contraseña <small>*</small></label>
-          <input name="password" className="form-control" type="text" />
-        </div>
-        <div>
-          <label name="password2">Repita Contraseña <small>*</small></label>
-          <input name="password2" className="form-control" type="text" />
-        </div>
-        <div className='d-flex justify-content-start align-items-baseline'>
-          <input name="rememberMe" type="checkbox" />
-          <label htmlFor='rememberMe' name="rememberMe" className='px-2'>Acepto termino y condiciones</label>
-        </div>
-        <div className='d-flex flex-row justify-content-around'>
-          <div>
-            <button type='submit' className='btn-toRegister' >Registarme</button>
-          </div>
-          <div>
-            <button type='submit' className='btn-toCancel' >Cancelar</button>
-          </div>
-        </div>
-        <div>
-          <small >¿Tenes una cuenta? <span className='info-register' onClick={() => setViewRegister(true)} >¡Logeate!</span></small>
-        </div>
-      </form>
-    </div>
+    </>
   )
 }
