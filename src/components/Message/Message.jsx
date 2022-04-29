1f619dbda9edc0a5f4d223fe25b2742782ce0824
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-28 23:22:28 -0300

    añade posicionamiento de texto

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const Message = ({message,setViewMessage,viewMessage}) => {
 
   return (
-    <div className={`alert alert-danger`}>
-        <button type='buttton' onClick={() =>setViewMessage(false)}>x</button>
+    <div className={`alert alert-danger d-flex justify-content-between`}>
         <strong>mensaje {message}</strong>
+        <button type='buttton' onClick={() =>setViewMessage(false)}>x</button>
     </div>
   )
 }
