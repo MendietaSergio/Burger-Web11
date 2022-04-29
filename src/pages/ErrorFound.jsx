0c618c2db92c370a0ef38e94d1481a897e52d29f
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-28 22:59:15 -0300

    Añade pagina no encontrada

---
+++ src/pages/ErrorFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ErrorFound = () => {
+  return (
+    <div className='container'>
+        <div className='d-flex justify-content-center'>
+            <h1 className='text-center my-5'>Página no encontrada</h1>
+        </div>
+    </div>
+  )
+}
