e4633051dd681c9e8a6382b3b3e32996c711b003
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-28 22:58:38 -0300

    Añade pagina de registro

---
+++ src/pages/RegisterPages.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Register } from '../components/Register/Register'
+
+export const RegisterPages = () => {
+    return (
+        <div className='container'>
+            <Register />
+        </div>
+    )
+}
