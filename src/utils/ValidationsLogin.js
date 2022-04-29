cb7e64b7952fc17fb06e1969e9078b6d78061f73
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-04-28 23:00:45 -0300

    Añade carpeta utils, donde estan las validaciones de login

---
+++ src/utils/ValidationsLogin.js
@@ -0,0 +1,22 @@
+export const validations = {
+    email: {
+        required: {
+            value: true,
+            message: "El mail es requerido."
+        },
+        pattern: {
+            value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            message: "El email debe ser válido."
+        }
+    },
+    password: {
+        required: {
+            value: true,
+            message: "La contraseña es requerida."
+        },
+        pattern: {
+            value: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/,
+            message: "El mail debe contener mayusculas, menisculas, números y simbolos."
+        }
+    }
+}
---
+++ src/utils/ValidationsRegister.js
@@ -0,0 +1,34 @@
+export const validations = {
+    nombre:{
+        required:{
+            value: true,
+            message:"El nombre es requerido"
+        }
+    },
+    usuario:{
+        required:{
+            value: true,
+            message:"El usuario es requerido",
+        }
+    },
+    email: {
+        required: {
+            value: true,
+            message: "El mail es requerido."
+        },
+        pattern: {
+            value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            message: "El email debe ser válido."
+        }
+    },
+    password: {
+        required: {
+            value: true,
+            message: "La contraseña es requerida."
+        },
+        pattern: {
+            value: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/,
+            message: "El mail debe contener mayusculas, menisculas, números y simbolos."
+        }
+    }
+}
