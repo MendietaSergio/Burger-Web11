ccb51a48629cb2a9d21c86b5e4cdd68be7f1828f
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-05-15 02:25:40 -0300

    Añade axios put para actualizar, dispatch y localstorage para poder almacenar los nuevos datos del usuario, se saca el campo contraseña para actualizar

--- src/components/containerProfile/PersonalInformation/PersonalInformation.jsx
+++ src/components/containerProfile/PersonalInformation/PersonalInformation.jsx
@@ -1,44 +1,58 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { types } from '../../../Types/Types'
 import { validations } from '../../../utils/ValidationsRegister'
 import { Message } from '../../Message/Message'
-import { Title } from '../../Title/Title'
+
 import './PersonalInformation.css'
-export const PersonalInformation = ({viewInformation}) => {
+export const PersonalInformation = ({ viewInformation, user, dispatch }) => {
     const { register, handleSubmit, reset, formState: { errors } } = useForm()
     const [viewMessage, setViewMessage] = useState(false)
     const [message, setMessage] = useState('')
     const [loading, setLoading] = useState(false)
     const [succes, setSucces] = useState(false)
+    const { nombre, email, _id, usuario, domicilio, description } = user;
     const submit = async (data) => {
         console.log(data);
-        //   setViewMessage(false)
-        //   setMessage('')
-        //   const { nombre, usuario, email, password } = data;
-        //   await axios.post('http://localhost:3001/api/auth/signup', {
-        //     nombre,
-        //     usuario,
-        //     email,
-        //     password
-        //   })
-        //     .then(res => {
-        //       setLoading(!loading)
-        //       if (res.data.ok) {
-        //         setTimeout(() => {
-        //           setSucces(true)
-        //           setLoading(false)
-        //         }, 2000)
+        setViewMessage(false)
+        setMessage('')
+        const { nombre, usuario, email, domicilio, description } = data;
+        await axios.put(`http://localhost:3001/api/user/${_id}`, {
+            nombre,
+            usuario,
+            email,
+            domicilio,
+            description
+        })
+            .then(res => {
+                setLoading(!loading)
+                if (res.data.ok) {
+                    localStorage.setItem('userBurger', JSON.stringify(res.data.logeado, null))
+                    handleLogin(res.data.logeado)
+                    setTimeout(() => {
+                        setSucces(true)
+                        setLoading(false)
+                    }, 2000)
 
-        //       } else {
-        //         setLoading(!loading)
-        //         setMessage(res.data.message)
-        //         setTimeout(() => {
-        //           setViewMessage(true)
-        //           setLoading(false)
-        //         }, 2000)
-        //       }
-        //     })
+                } else {
+                    setLoading(!loading)
+                    setMessage(res.data.message)
+                    setTimeout(() => {
+                        setViewMessage(true)
+                        setLoading(false)
+                    }, 2000)
+                }
+            })
     }
+    const handleLogin = (data) => {
+        dispatch({
+          type: types.login,
+          payload: {
+            ...data
+          }
+        })
+      }
     if (viewInformation) {
 
         return (
@@ -59,7 +73,7 @@ export const PersonalInformation = ({viewInformation}) => {
                                     className={errors.nombre ? ("form-control is-invalid") : ("form-control")}
                                     type="text"
                                     {...register('nombre', validations.nombre)}
-                                    defaultValue='nombre'
+                                    defaultValue={nombre}
                                 />
                                 {errors.nombre ? <small className='text-danger'>{errors.nombre.message}</small> : null}
                             </div>
@@ -69,7 +83,7 @@ export const PersonalInformation = ({viewInformation}) => {
                                     name="usuario"
                                     className="form-control"
                                     type="text"
-                                    defaultValue='nombre'
+                                    defaultValue={usuario}
                                     disabled='true'
                                 />
                             </div>
@@ -79,27 +93,32 @@ export const PersonalInformation = ({viewInformation}) => {
                                     name="email"
                                     className="form-control"
                                     type="email"
-                                    defaultValue='nombre'
+                                    defaultValue={email}
                                     disabled='true'
                                 />
                             </div>
-                            <div className='col-12 col-md-6'>
-                                <label name="password">Contraseña <small>*</small></label>
+                            {/* <div className='col-12 col-md-6'>
+                                <label name="password">Contraseña <small>*</small>
+                                </label>
                                 <input
                                     name="password"
                                     className={errors.password ? ("form-control is-invalid") : ("form-control")} type="password"
-                                    {...register('password', validations.password)}
+                                    {value.length>0 ? }
+                                    // {...register('password', validations.password)}
                                 />
                                 {errors.password ? <small className='text-danger'>{errors.password.message}</small> : null}
-                            </div>
+                            </div> */}
                         </div>
                         <div className='row'>
                             <div className='col-12 col-md-8'>
-                                <label name="domicilio">Domicilio <small>*</small></label>
+                                <label name="domicilio">Domicilio
+                                    {domicilio === undefined ? <small> *</small> : (null)}
+                                </label>
                                 <input
                                     name="domicilio"
                                     className={errors.domicilio ? ("form-control is-invalid") : ("form-control")}
                                     type="text"
+                                    defaultValue={domicilio}
                                     {...register('domicilio', validations.domicilio)}
                                 />
                                 {errors.domicilio ? <small className='text-danger'>{errors.domicilio.message}</small> : null}
@@ -112,7 +131,9 @@ export const PersonalInformation = ({viewInformation}) => {
                                     maxLength="500"
                                     minLength="20"
                                     placeholder='Deje detalles del domicilio...'
-                                    {...register('description', validations.description)} />
+                                    {...register('description', validations.description)}
+                                    defaultValue={description ? description : null}
+                                />
                                 {errors.description ? <small className='text-danger'>{errors.description.message}</small> : null}
                             </div>
                         </div>
@@ -127,7 +148,7 @@ export const PersonalInformation = ({viewInformation}) => {
                 </div >
             </>
         )
-    } else{
+    } else {
         return null
     }
 }
