0425a6efc304c83e0370e6111ac10c689cb6cb15
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-05-15 02:23:25 -0300

    Añade info mostrado desde context

--- src/components/containerProfile/Imgprofile/ImgProfile.jsx
+++ src/components/containerProfile/Imgprofile/ImgProfile.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './ImgProfile.css'
-export const ImgProfile = () => {
+export const ImgProfile = ({avatar,nombre, usuario}) => {
     return (
         <div className="col-12 col-md-4">
             <div className='container-profile m-3 py-5 '>
-                <img src='https://img.freepik.com/vector-gratis/astronauta-lindo-que-come-ilustracion-icono-vector-historieta-hamburguesa-icono-comida-espacial_138676-1988.jpg?t=st=1649332235~exp=1649332835~hmac=8ca6aec4084825b695e9bd0bbf7ca93d961ff48ceb37f565444afa3598f73e6c&w=740' alt="perfil" />
-                <h1>Usuario</h1>
-                <span>Nombre completo</span>
+                <img src={avatar} alt="perfil" />
+                <h4>{usuario}</h4>
+                <span>{nombre}</span>
             </div>
         </div>
 
