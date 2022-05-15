7ae16154f6a6d73d2bf1daadf3672a18e0864835
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-05-15 20:30:24 -0300

    Añade vista de lista de opciones, dependiendo el rol

--- src/components/containerProfile/OptionProfile/ViewOptions/ViewOptions.jsx
+++ src/components/containerProfile/OptionProfile/ViewOptions/ViewOptions.jsx
@@ -1,36 +1,46 @@
 import React from 'react'
 import './ViewOptions.css'
-export const ViewOptions = ({widthImg, admin,view}) => {
+export const ViewOptions = ({ widthImg, admin, view }) => {
+    console.log("admin => ", admin[0].name);
 
     if ('550' >= widthImg) {
-    return (
-        <>
-            <select name="filter" className='form-select select-option-account' onChange={(e) => view(e.target.value)}>
-                <option value="personal">Datos personales</option>
-                <option value="history">Historia</option>
-                {admin ? (
-                    <>
-                        <option value="order">Ordenes de compras</option>
-                        <option value="usuario">Nuevo usuario</option>
-                        <option value="clientes">Lista de clientes</option>
-                    </>
-                ) : null}
-            </select>
-        </>
-    )
+        return (
+            <>
+                <select name="filter" className='form-select select-option-account' onChange={(e) => view(e.target.value)}>
+                    {admin[0].name === 'admin' ? (
+                        <>
+                            <option value="personal">Datos personales</option>
+                            <option value="order">Ordenes de compras</option>
+                            <option value="usuario">Nuevo usuario</option>
+                            <option value="articulo">Nuevo articulo</option>
+                            <option value="clientes">Lista de clientes</option>
+                        </>
+                    ) : (
+                        <>
+                            <option value="personal">Datos personales</option>
+                            <option value="history">Historial</option>
+                        </>
+                    )}
+                </select>
+            </>
+        )
     } else {
         return (
             <ul className='list-inline mb-0'>
-                <li className='list-inline-item' onClick={() => view("personal")}>Datos personales</li>
-                <li className='list-inline-item' onClick={() => view('history')}>Historial</li>
-                {admin ? (
+                {admin[0].name === 'admin' ? (
                     <>
+                        <li className='list-inline-item' onClick={() => view("personal")}>Datos personales</li>
                         <li className='list-inline-item' onClick={() => view('order')}>Ordenes de compras</li>
                         <li className='list-inline-item' onClick={() => view('usuario')}>Nuevo usuario</li>
                         <li className='list-inline-item' onClick={() => view('articulo')}>Nuevo articulo</li>
                         <li className='list-inline-item' onClick={() => view('clientes')}>Lista de clientes</li>
                     </>
-                ) : (null)}
+                ) : (
+                    <>
+                        <li className='list-inline-item' onClick={() => view("personal")}>Datos personales</li>
+                        <li className='list-inline-item' onClick={() => view('history')}>Historial</li>
+                    </>
+                )}
             </ul>
         )
     }
