0dff8eab35ed049b731b10b665e43967b2441ec2
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-05-15 02:27:27 -0300

    Añade usecontext, authcontext, se pasan por props datos a mostrar en componentes

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../Auth/AuthContext'
 import { ImgProfile } from '../components/containerProfile/Imgprofile/ImgProfile'
 import { OptionProfile } from '../components/containerProfile/OptionProfile/OptionProfile'
 
 export const Myaccount = () => {
+  const { user, dispatch } = useContext(AuthContext)
+  const { nombre, usuario, avatar} = user;
   return (
     <div>
       <div className='container'>
         <div className="row">
-          <ImgProfile />
-          <OptionProfile />
+          <ImgProfile
+            avatar={avatar}
+            nombre={nombre}
+            usuario={usuario} />
+          <OptionProfile
+            user={user}
+            dispatch={dispatch}
+          />
         </div>
 
       </div>
