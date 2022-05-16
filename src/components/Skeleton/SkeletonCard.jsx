7fbf358525b3a3263d58792021c17d5cda30cf3c
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-05-16 18:31:43 -0300

    Añade skeleton del detalle del producto, img description

--- src/components/Skeleton/SkeletonCard.jsx
+++ src/components/Skeleton/SkeletonCard.jsx
@@ -1,30 +1,84 @@
 import React from 'react'
 import './SkeletonCard.css'
-export const SkeletonCard = ({ loading }) => {
-  const cant = []
+export const SkeletonCard = ({
+  loading,
+  skeletonImg = false,
+  product = false,
+  skeletonDetail = false,
+  className = "image",
+  viewDescription = false
+}) => {
+  if (skeletonImg) {
+    return (
 
-  for (let i = 1; i <= 6; i++) {
-    cant.push(i)
+      <div className='card loading margin my-1'>
+        <div className={className}>
+
+        </div>
+      </div>
+    )
   }
-
-  return (
-    <>
-      {cant.map((card, index) => (
-        <div className="col-12 col-md-4 col-lg-3 my-2" key={index}>
-          <div className='card loading'>
-            <div className='image'>
-
-            </div>
-            <div className="content">
+  if (skeletonDetail) {
+    const cant = []
+    for (let i = 1; i <= 5; i++) {
+      cant.push(i)
+    }
+    return (
+      <div className='card loading margin my-1'>
+        <div className="content">
+          {viewDescription ? (
+            <>
               <h4></h4>
-              <div className="details">
+            </>
+          ) : (
+            <>
+              <h1></h1>
+              <h4></h4>
+              <div className="price">
 
+              </div>
+            </>
+
+          )
+          }
+          {
+            cant.map(list => (
+              <div className="list">
+
+              </div>
+            ))
+          }
+        </div >
+      </div >
+    )
+  }
+  if (product) {
+    const cant = []
+
+    for (let i = 1; i <= 6; i++) {
+      cant.push(i)
+    }
+
+    return (
+      <>
+        {cant.map((card, index) => (
+          <div className="col-12 col-md-4 col-lg-3 my-2" key={index}>
+            <div className='card loading'>
+              <div className='image'>
+
+              </div>
+              <div className="content">
+                <h4></h4>
+                <div className="details">
+
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))
-      }
-    </>
-  )
+        ))
+        }
+      </>
+    )
+  }
+
 }
