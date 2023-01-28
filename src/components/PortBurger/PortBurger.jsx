3604f2dcc491b60b44a3f274db140f5e9a01bd1c
MendietaSergio <mendieta.sergio.1995@gmail.com>
2023-01-28 20:04:11 -0300

    UPDATE: clear code

--- src/components/PortBurger/PortBurger.jsx
+++ src/components/PortBurger/PortBurger.jsx
@@ -1,31 +1,10 @@
-import React,{ useState, useEffect }  from 'react'
+import React from 'react'
 
 import './PortBurger.css'
-export const PortBurger = () => {
-    // const useScreenSize = () => {
-        const [ widthImg, setWidthImg] = useState(window.innerWidth)
-      
-        useEffect(() => {
-          window.addEventListener("resize", handleResize);
-      
-          return () => {
-            window.removeEventListener("resize", handleResize);
-          };
-        }, [window.innerWidth]);
-      
-        const handleResize = () => {
-          setWidthImg(window.innerWidth);
-          console.log("Tamaño de la pantalla => ",widthImg);
-        };
-      
-        // return { widthImg };
-    //   };
-    // useEffect(() =>{
-    //     useScreenSize()
-    // },[])
+export const PortBurger = ({ widthImg }) => {
   return (
     <div className='container_img'>
-        <img width={widthImg} height='350' className='img_port' src="https://res.cloudinary.com/freelance01/image/upload/v1648490237/burger_web/Portada_burger.2_yannxf.webp" alt="Portada" />
+      <img width={widthImg} height='350' className='img_port' src="https://res.cloudinary.com/freelance01/image/upload/v1648490237/burger_web/Portada_burger.2_yannxf.webp" alt="Portada" />
     </div>
   )
 }
