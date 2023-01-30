1264e31610ca78e02d3b0512ae75655996225c94
MendietaSergio <mendieta.sergio.1995@gmail.com>
2023-01-30 18:51:35 -0300

    ADD: context cart

--- src/Context/CartContextProvider.jsx
+++ src/Context/CartContextProvider.jsx
@@ -6,7 +6,7 @@ export const CartContextUse = () => {
     return useContext(CartContext)
 }
 
-export const CartContextProvider = ({ Children }) => {
+export const CartContextProvider = ({ children }) => {
     const [cart, setCart] = useState([])
     const [orderId, setOrderId] = useState()
     const orders = (formData, priceTotal) => {
@@ -15,38 +15,42 @@ export const CartContextProvider = ({ Children }) => {
         order.buyer = formData;
         order.total = priceTotal;
         order.items = cart.map((cartItem) => {
-            const id = cartItem.item.id;
-            const nameProduct = cartItem.item.nameProduct;
-            const price = cartItem.item.price * cartItem.quantity
-            return { id, nameProduct, price }
+            const _id = cartItem.item._id;
+            const nombre = cartItem.item.nombre;
+            const precio = cartItem.item.precio * cartItem.cantidad
+            return { _id, nombre, precio }
         })
     }
-    const addItem = (item, quantity) => {
-        if (isInCart(item.id)) {
+    const addItem = (item, cantidad) => {
+        if (isInCart(item._id)) {
             const updateQty = [...cart];
             updateQty.map(element => {
-                if (element.item.id === item.id) {
-                    element.quantity = element.quantity + quantity
+                if (element.item._id === item._id) {
+                    if (cantidad >= 1) {
+                        element.cantidad = cantidad;
+                    } else {
+                        element.cantidad = element.cantidad + cantidad
+                    }
                 }
                 return element
             })
             setCart(updateQty)
         }
         else {
-            setCart([...cart, { item, quantity }])
+            setCart([...cart, { item, cantidad }])
         }
     }
     //SI ESTA EN EL CARRITO, SUMO SU CANTIDAD
-    const isInCart = (id) => cart.find(element => element.item.id === id)
+    const isInCart = (_id) => cart.find(element => element.item._id === _id)
     //BORRAR DE LISTA DEL CARRITO
     const clear = () => setCart([])
     //ELIMINO EL ITEM SELECCIONADO
-    const removeItem = (id) => {
-        const cartFilter = cart.filter(element => element.item.id !== id)
+    const removeItem = (_id) => {
+        const cartFilter = cart.filter(element => element.item._id !== _id)
         setCart(cartFilter)
     }
     const iconCart = () => {
-        return cart.reduce((acum, valor) => acum + valor.quantity, 0)
+        return cart.reduce((acum, valor) => acum + valor.cantidad, 0)
     }
     return (
         <CartContext.Provider value={{ cart, addItem, clear, removeItem, iconCart, orders, orderId, setOrderId }}>
