280ac6d9b427e5a5a4102f7fcc6923e074e58b1d
MendietaSergio <mendieta.sergio.1995@gmail.com>
2023-01-30 18:54:36 -0300

    UPDATE: dependencies, code context header cart, routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,41 +13,57 @@ import { Myaccount } from './pages/Myaccount'
 import { ErrorFound } from './pages/ErrorFound'
 import { AuthContext } from './Auth/AuthContext'
 import { AuthReducer } from './Auth/AuthReducer'
+import { CartContextProvider } from './Context/CartContextProvider'
 
-const init = () =>{
+const init = () => {
   return JSON.parse(localStorage.getItem('userBurger')) || {
-    logueado : false
+    logueado: false
   }
 }
 
 function App() {
-  const [user, dispatch] = useReducer(AuthReducer, { }, init)
 
-  useEffect(() =>{
+  const [user, dispatch] = useReducer(AuthReducer, {}, init)
+
+  useEffect(() => {
     localStorage.setItem("userBurger", JSON.stringify(user, null, 3))
-  },[user])
+  }, [user])
+  const [widthImg, setWidthImg] = useState(window.innerWidth)
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [window.innerWidth]);
+
+  const handleResize = () => {
+    setWidthImg(window.innerWidth);
+  };
   return (
-    <AuthContext.Provider 
-      value={{user, dispatch}}
+    <AuthContext.Provider
+      value={{ user, dispatch }}
     >
-      <BrowserRouter>
-        <div className="container-fluid">
-          <Header />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/productos' element={<Productos  />} />
-            <Route path='/productos/:idCategoria' element={<Productos  />} />
-            <Route path='/productos/:idCategoria/:idSubcategoria' element={<Productos  />} />
-            <Route path='/productos/detalle/:idDetail' element={<Detail />} />
-            <Route path='/ingresar' element={<LoginPages />} />
-            <Route path='/registrarse' element={<RegisterPages />} />
-            <Route path='/micuenta' element={<Myaccount />} />
-            <Route path='/micarrito' element={<Cart />} />
-            <Route path='*' element={<ErrorFound />} />
-          </Routes>
-          <Footer />
-        </div>
-      </BrowserRouter>
+      <CartContextProvider>
+        <BrowserRouter>
+          <div className="container-fluid">
+            <Header setWidthImg={setWidthImg} widthImg={widthImg} />
+            <Routes>
+              <Route path='/' element={<Home setWidthImg={setWidthImg} widthImg={widthImg} />} />
+              <Route path='/productos' element={<Productos cantPages={false} />} />
+              <Route path='/productos/:idCategoria' element={<Productos />} />
+              <Route path='/productos/:idCategoria/:idSubcategoria' element={<Productos />} />
+              <Route path='/productos/detalle/:idDetail' element={<Detail />} />
+              <Route path='/ingresar' element={<LoginPages />} />
+              <Route path='/registrarse' element={<RegisterPages />} />
+              <Route path='/micuenta' element={<Myaccount />} />
+              <Route path='/micarrito' element={<Cart />} />
+              <Route path='*' element={<ErrorFound />} />
+            </Routes>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </CartContextProvider>
     </AuthContext.Provider>
   )
 }
