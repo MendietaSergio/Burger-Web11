0cb6944ed38fdcc2e44690f6eb6d207819e9fece
MendietaSergio <mendieta.sergio.1995@gmail.com>
2023-02-03 15:45:45 -0300

    ADD: options

---
+++ src/utils/OptionsProfile.js
@@ -0,0 +1,30 @@
+export const optionsProfile = [
+    {
+        name: 'personal',
+        option: true,
+    },
+    {
+        name: 'order',
+        option: false,
+    },
+    {
+        name: 'history',
+        option: false,
+    },
+    {
+        name: 'usuario',
+        option: false,
+    },
+    {
+        name: 'articulo',
+        option: false,
+    },
+    {
+        name: 'listaarticulo',
+        option: false,
+    },
+    {
+        name: 'clientes',
+        option: false,
+    },
+]
