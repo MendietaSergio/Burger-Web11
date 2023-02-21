33d668b1bd6d25c341804f8084e3429252900798
MendietaSergio <mendieta.sergio.1995@gmail.com>
2023-02-21 09:42:17 -0300

    UPDATE: options selection home

--- src/components/OptionsSelection/OptionsSelection.jsx
+++ src/components/OptionsSelection/OptionsSelection.jsx
@@ -10,7 +10,6 @@ export const OptionsSelection = () => {
                         <div className='container-optionSelection my-3'>
                             <img className='img-optionsSelection' src="https://res.cloudinary.com/freelance01/image/upload/v1648526368/burger_web/burgers_option_jlzycs.jpg" alt="Burger" />
                             <div className='container-titleOptions'>
-                                <h1 className='titleOptions'>Las mejores burgers</h1>
                                 <div className='container-optionsLink'>
                                     <Link className='optionsLink' to="/productos/burger">Ver más <i className="fas fa-hamburger"></i></Link>
                                 </div>
@@ -21,7 +20,6 @@ export const OptionsSelection = () => {
                         <div className='container-optionSelection my-3'>
                             <img className='img-optionsSelection' src="https://res.cloudinary.com/freelance01/image/upload/v1648526368/burger_web/papas_option_hcjilg.jpg" alt="Papas" />
                             <div className='container-titleOptions'>
-                                <h1 className='titleOptions'>acompaña tus papas con tu salsa favorita</h1>
                                 <div className='container-optionsLink'>
                                     <Link className='optionsLink' to="/productos/papasysalsas">Ver más <i className="fas fa-utensils"></i></Link>
                                 </div>
@@ -32,7 +30,6 @@ export const OptionsSelection = () => {
                         <div className='container-optionSelection my-3'>
                             <img className='img-optionsSelection' src="https://res.cloudinary.com/freelance01/image/upload/v1648526370/burger_web/Agua-saborizada_option_fgxkt0.png" alt="Bebida" />
                             <div className='container-titleOptions'>
-                                <h1 className='titleOptions'>bebidas para acompañar</h1>
                                 <div className='container-optionsLink'>
                                     <Link className='optionsLink' to="/productos/bebidas">Ver más <i className="fas fa-glass-whiskey"></i></Link>
                                 </div>
