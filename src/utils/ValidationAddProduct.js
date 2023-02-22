b102ae2812075f00c6d8417f368fba58493db003
Sergio Mendieta <mendieta.sergio.1995@gmail.com>
2023-02-21 22:33:01 -0300

    ADD: validate.tipo

--- src/utils/ValidationAddProduct.js
+++ src/utils/ValidationAddProduct.js
@@ -1,83 +1,90 @@
 export const ValidationAddProduct = {
-    nombre: {
-        required: {
-            value: true,
-            message: "El nombre del producto es requerido."
-        }
+  nombre: {
+    required: {
+      value: true,
+      message: "El nombre del producto es requerido.",
     },
-    precio: {
-        required: {
-            value: true,
-            message: "El precio es requerido."
-        },
-        min: {
-            value: 30,
-            message: "Debe ser mayor a 30"
-        }
+  },
+  precio: {
+    required: {
+      value: true,
+      message: "El precio es requerido.",
     },
+    min: {
+      value: 30,
+      message: "Debe ser mayor a 30",
+    },
+  },
 
-    descuento: {
-        minLength: {
-            value: 10,
-            message: "Debe tener minimo 10 carácteres"
-        },
-        maxLength: {
-            value: 30,
-            message: "Debe tener maximo 30 carácteres"
-        }
+  descuento: {
+    minLength: {
+      value: 10,
+      message: "Debe tener minimo 10 carácteres",
     },
-    categoria: {
-        required: {
-            value: true,
-            message: "La categoria es obligatorio"
-        }
+    maxLength: {
+      value: 30,
+      message: "Debe tener maximo 30 carácteres",
     },
-    subcategoria: {
-        required: {
-            value: true,
-            message: "La subcategoria es obligatorio"
-        }
+  },
+  categoria: {
+    required: {
+      value: true,
+      message: "La categoria es obligatorio",
     },
-    descripcion: {
-        minLength: {
-            value: 20,
-            message: "Debe tener al menos 5 carácteres."
-        },
-        maxLength: {
-            value: 100,
-            message: "El máximo son 100 carácteres."
-        },
+  },
+  subcategoria: {
+    required: {
+      value: true,
+      message: "La subcategoria es obligatorio",
     },
-    ingredientes: {//VER MAS ADELANTE SI DEJARLO REQUERIDO O NO, EN BASE A SU CATEGORIA
-        minLength: {
-            value: 5,
-            message: "Debe tener al menos 5 carácteres."
-        },
-        maxLength: {
-            value: 100,
-            message: "El máximo son 100 carácteres."
-        },
+  },
+  descripcion: {
+    minLength: {
+      value: 20,
+      message: "Debe tener al menos 5 carácteres.",
     },
-    img: {
-        required: {
-            value: true,
-            message: "La imagen es requerida"
-        }
+    maxLength: {
+      value: 100,
+      message: "El máximo son 100 carácteres.",
     },
-    oferta: {
-        required: {
-            value: true,
-            message: "La oferta es requerida"
-        }
+  },
+  ingredientes: {
+    //VER MAS ADELANTE SI DEJARLO REQUERIDO O NO, EN BASE A SU CATEGORIA
+    minLength: {
+      value: 5,
+      message: "Debe tener al menos 5 carácteres.",
     },
-    descuento: {
-        required: {
-            value: true,
-            message: "El descuento es requerido"
-        },
-        min: {
-            value: 5,
-            message: "El minimo de descuento es de 5%."
-        }
-    }
-}
+    maxLength: {
+      value: 100,
+      message: "El máximo son 100 carácteres.",
+    },
+  },
+  img: {
+    required: {
+      value: true,
+      message: "La imagen es requerida",
+    },
+  },
+  oferta: {
+    required: {
+      value: true,
+      message: "La oferta es requerida",
+    },
+  },
+  descuento: {
+    required: {
+      value: true,
+      message: "El descuento es requerido",
+    },
+    min: {
+      value: 5,
+      message: "El minimo de descuento es de 5%.",
+    },
+  },
+  tipo: {
+    required: {
+      value: true,
+      message: "El tipo es requerido",
+    },
+  },
+};
