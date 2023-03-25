586276d557314289ef1c2f9b633ec8784341ad6f
MendietaSergio <mendieta.sergio.1995@gmail.com>
2023-03-25 17:46:26 -0300

    UPDATE: not found pages

--- src/pages/ErrorFound.jsx
+++ src/pages/ErrorFound.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import './ErrorFound.css'
+import imgNotFound from '../Img/NotFound.png'
+import { Link } from 'react-router-dom'
 export const ErrorFound = () => {
   return (
     <div className='container'>
-        <div className='d-flex justify-content-center'>
-            <h1 className='text-center my-5'>Página no encontrada</h1>
-        </div>
+      <div className='container-notPages d-flex justify-content-center'>
+        <h1 className='text-center my-5'>Página no encontrada</h1>
+        <img className='imgNotFound my-4' src={imgNotFound} alt="notFound" />
+        <ul className='list-enlaces'>
+          <li><Link className="nav-link" to="/">Inicio</Link></li>
+          <li><Link className="nav-link" to="/productos" >Productos</Link></li>
+        </ul>
+      </div>
     </div>
   )
 }
